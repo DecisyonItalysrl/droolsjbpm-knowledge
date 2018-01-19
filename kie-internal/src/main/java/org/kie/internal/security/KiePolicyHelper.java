f527ebc99d59474e3d170f0e0e92c15661cdb909
emanuela iacobone <emanuela.iacobone@decisyon.com>
2018-01-19 09:35:43 +0100

    Merged with version 6.3.1-decisyon to use policy from http

--- kie-internal/src/main/java/org/kie/internal/security/KiePolicyHelper.java
+++ kie-internal/src/main/java/org/kie/internal/security/KiePolicyHelper.java
@@ -11,12 +11,14 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
-*/
+ */
+/* This class has been modify by Decisyon Italy.
+ * Policy from http can be used.
+ */
 
 package org.kie.internal.security;
 
 import java.io.File;
-import java.net.URI;
 import java.security.AccessControlContext;
 import java.security.CodeSource;
 import java.security.PermissionCollection;
@@ -25,35 +27,46 @@ import java.security.ProtectionDomain;
 import java.security.URIParameter;
 import java.security.cert.Certificate;
 
+import org.kie.internal.security.util.SecurityPolicyUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class KiePolicyHelper {
 
-    private static final Logger               log                          = LoggerFactory.getLogger(KiePolicyHelper.class);
+    private static final Logger log = LoggerFactory.getLogger(KiePolicyHelper.class);
 
-    public static final String                KIE_SECURITY_POLICY_PROPERTY = "kie.security.policy";
-    private static final boolean              policyEnabled;
+    public static final String KIE_SECURITY_POLICY_PROPERTY = "kie.security.policy";
+    private static final String KIE_SECURITY_POLICY_USERNAME = "KIE_SECURITY_USERNAME";
+    private static final String KIE_SECURITY_POLICY_PASSWORD = "KIE_SECURITY_PASSWORD";
+
+    private static final boolean policyEnabled;
     private static final AccessControlContext context;
 
     static {
         AccessControlContext ctx = null;
         try {
-            String policyFile = System.getProperty(KIE_SECURITY_POLICY_PROPERTY);
-            if (policyFile != null) {
-                log.info("Kie policy file property defined: " + policyFile);
+            String policyURL = System.getProperty(KIE_SECURITY_POLICY_PROPERTY);
+            if (policyURL != null) {
+                log.info("Kie policy file property defined: " + policyURL);
             }
             SecurityManager securityManager = System.getSecurityManager();
-            if (policyFile != null && securityManager == null) {
-                log.warn("Security manager not started. The KIE policy file configuration will be ignored. In order to use the policy file, a security manager needs to be started.");
+            if (policyURL != null && securityManager == null) {
+                log.warn(
+                        "Security manager not started. The KIE policy file configuration will be ignored. In order to use the policy file, a security manager needs to be started.");
             }
-            if (policyFile != null && securityManager != null) {
-                URI resource = new File(policyFile).toURI();
-                Policy instance = Policy.getInstance("JavaPolicy", new URIParameter(resource));
+            if (policyURL != null && securityManager != null) {
+
+                String userName = System.getenv(KIE_SECURITY_POLICY_USERNAME);
+                String password = System.getenv(KIE_SECURITY_POLICY_PASSWORD);
+
+                File policyfileT = SecurityPolicyUtil.getPolicyFromHTTP(policyURL, userName, password);
+
+                Policy instance = Policy.getInstance("JavaPolicy", new URIParameter(policyfileT.toURI()));
                 PermissionCollection permissions = instance.getPermissions(new CodeSource(null, (Certificate[]) null));
-                ProtectionDomain[] pds = {new ProtectionDomain(new CodeSource(null, (Certificate[]) null), permissions)};
+                ProtectionDomain[] pds = {
+                        new ProtectionDomain(new CodeSource(null, (Certificate[]) null), permissions) };
                 ctx = new AccessControlContext(pds);
-                log.info("Kie policy successfuly loaded and installed.");
+                log.info("Kie policy successfuly loaded from http resource and installed");
             } else {
                 ctx = null;
             }
